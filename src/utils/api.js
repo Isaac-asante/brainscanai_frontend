919dd2ba46b84b1df52f79cd86e77aa13892e255
Isaac-asante <iaasante002@st.ug.edu.gh>
2025-09-18 06:59:37 +0100

    Fix path issue, successful build

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import toast from 'react-hot-toast'
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000'
+const API_BASE_URL = import.meta.env.VITE_API_URLL || 'http://127.0.0.1:5000'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
